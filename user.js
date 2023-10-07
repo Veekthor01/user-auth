a4b6cac050113f187f6523cacbaeb88762bc7b61
Veekthor01 <veekthor01@gmail.com>
2023-10-07 14:06:40 +0300

    add user functions

--- user.js
+++ user.js
@@ -1,11 +1,28 @@
 import bcrypt from 'bcrypt';
 import { ObjectId } from 'mongodb';
 import { connectToDB } from './db.js';
+import "./passport.js";
+
+// Function to save a new user to the database
+export async function saveUser(newUser) {
+    const db = await connectToDB();
+    const usersCollection = db.collection('user');
+    try {
+      // Insert the new user document into the users collection
+      const result = await usersCollection.insertOne(newUser);
+        // Return the newly saved user
+        return result;
+    } catch (error) {
+      // Handle the error, e.g., log it or return an error response
+      console.error('Error saving user:', error);
+      throw error;
+    }
+  }
 
 // Function to get a user by ID from the database
 export async function getUserById(id) {
   const db = await connectToDB();
-  const usersCollection = db.collection('users');
+  const usersCollection = db.collection('user');
   const user = await usersCollection.findOne({ _id: new ObjectId(id) });
   return user;
 }
@@ -13,7 +30,7 @@ export async function getUserById(id) {
 // Function to get a user by email from the database
 export async function getUserByEmail(email) {
   const db = await connectToDB();
-  const usersCollection = db.collection('users');
+  const usersCollection = db.collection('user');
   const user = await usersCollection.findOne({ email: email });
   return user;
 }
@@ -27,15 +44,9 @@ export async function validatePassword(user, password) {
   return passwordMatch;
 }
 
-// Function to save a new user to the database
-export async function saveUser(newUser) {
-    const db = await connectToDB();
-    const usersCollection = db.collection('users');
-    // Insert the new user document into the users collection
-    const result = await usersCollection.insertOne(newUser);
-    if (result.insertedCount === 1) {
-      return result.ops[0]; // Return the newly inserted user document
-    } else {
-      throw new Error('Failed to save user');
-    }
-  }
+  
+  
+  
+  
+  
+  
