5554a63e4880a0a9b3c4ee7447a471245838ec25
Veekthor01 <veekthor01@gmail.com>
2023-10-07 19:25:27 +0300

    add objectid module

--- user.js
+++ user.js
@@ -17,32 +17,42 @@ export async function saveUser(newUser) {
       console.error('Error saving user:', error);
       throw error;
     }
-  }
+  };
+
+// Function to get a user by email from the database
+export async function getUserByEmail(email) {
+    const db = await connectToDB();
+    const usersCollection = db.collection('user');
+    try {
+        const user = await usersCollection.findOne({ email: email });
+        return user;
+    } catch (error) {
+        console.error('Error getting user by email:', error);
+        throw error;
+    }
+};
 
 // Function to get a user by ID from the database
 export async function getUserById(id) {
   const db = await connectToDB();
   const usersCollection = db.collection('user');
-  const user = await usersCollection.findOne({ _id: new ObjectId(id) });
-  return user;
-}
-
-// Function to get a user by email from the database
-export async function getUserByEmail(email) {
-  const db = await connectToDB();
-  const usersCollection = db.collection('user');
-  const user = await usersCollection.findOne({ email: email });
-  return user;
-}
+    try {
+        const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+        return user;
+    } catch (error) {
+        console.error('Error getting user by ID:', error);
+        throw error;
+    }
+};
 
 // Function to validate a user's password
 export async function validatePassword(user, password) {
-  if (!user) {
-    return false; // User not found
-  }
-  const passwordMatch = await bcrypt.compare(password, user.passwordHash);
-  return passwordMatch;
-}
+    if (!user) {
+      return false; // User not found
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    return passwordMatch;
+  };  
 
   
   
