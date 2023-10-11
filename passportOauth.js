8f84c81ca9b9f3c5791ca0d0ed813e6357bdbc75
Veekthor01 <veekthor01@gmail.com>
2023-10-11 19:15:42 +0300

    add google oauth

--- passportOauth.js
+++ passportOauth.js
@@ -1,10 +1,13 @@
 import passport from 'passport';
 import { Strategy as GithubStrategy } from 'passport-github2'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 
 passport.use(
@@ -18,6 +21,19 @@ passport.use(
             return done(null, profile);
     } 
   )
+);
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            callbackURL: 'http://localhost:3000/auth/google/callback',
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+        }
+    )
 );
 
 passport.serializeUser(function(user, done) {
