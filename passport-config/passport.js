b23579d60bd0737ec3013f9e63b8136d75dc42ed
Veekthor01 <veekthor01@gmail.com>
2023-10-13 22:55:19 +0300

    add passport config to folder

---
+++ passport-config/passport.js
@@ -0,0 +1,86 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy;
+const { getUserById, getUserByEmail, saveUser } = require('../user');
+
+
+// Function to generate a password hash
+async function generateHash(password) {
+  const saltRounds = 10; // You can adjust the number of salt rounds for security
+  return await bcrypt.hash(password, saltRounds);
+}
+
+// Local authentication strategy for signup (username/password)
+passport.use(
+  'local-signup',
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true, // Pass the req object to the callback below
+    },
+    async (req, email, password, done) => {
+      try {
+        // Check if the email is already taken
+        const user = await getUserByEmail(email);
+        if (user) {
+          req.flash('error', 'Email is already taken.');
+          return done(null, false);
+        }
+        const hashedPassword = await generateHash(password);
+        const newUser = {
+          email: email,
+          password: hashedPassword,
+        };
+        // Save the new user to your database
+        const savedUser = await saveUser(newUser);
+        return done(null, savedUser);
+      } catch (err) {
+        req.flash('error', 'An error occurred during signup.');
+        return done(err);
+      }
+    }
+  )
+);
+
+// Local authentication strategy (username/password)
+passport.use(
+    'local',
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true, // Pass the req object to the callback below
+    },
+  async (req, email, password, done) => {
+    try {
+      const user = await getUserByEmail(email);
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+// Serialization and deserialization functions to store user data in sessions
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+ passport.deserializeUser(async (id, done) => {
+    try {
+     const user = await getUserById(id);
+        done(null, user);
+    } catch (err) {
+     done(err);
+    }
+     });
+
+module.exports = passport;
---
+++ passport-config/passportOauth.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const GithubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+            callbackURL: 'http://localhost:3000/auth/github/callback',
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+    } 
+  )
+);
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            callbackURL: 'http://localhost:3000/auth/google/callback',
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+        }
+    )
+);
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+   });
+   passport.deserializeUser(function(user, done) {
+    done(null, user);
+   });
+
+module.exports = passport;
