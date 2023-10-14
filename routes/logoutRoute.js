0640943e6a38d3644883e1faa24c6d4cf9af75a8
Veekthor01 <veekthor01@gmail.com>
2023-10-14 14:48:30 +0300

    update route

--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import { Router } from "express";
-
+const express = require('express');
 const logoutRouter = express.Router();
 
 logoutRouter.get('/', (req, res) => {
-    req.logout();
-    req.flash("success", "Logout successful");
-    return res.redirect('/');
-  });
+  try {
+      req.logout();
+      req.flash('success', 'Logout successful');
+      return res.redirect('/');
+  } catch (error) {
+      // Handle the error, for example, by sending an error response or logging it.
+      console.error('An error occurred:', error);
+      return res.status(500).send('An error occurred while logging out.');
+  }
+});
 
-export default logoutRouter;
+module.exports = logoutRouter;
