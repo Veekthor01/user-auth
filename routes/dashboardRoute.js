171c9bd380d6c6ae7cc061cd0cf8dd37a8caef6b
Veekthor01 <veekthor01@gmail.com>
2023-10-14 14:46:37 +0300

    update route

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,11 +1,15 @@
-import express from 'express';
-import { Router } from "express";
-
+const express = require('express');
 const dashboardRouter = express.Router();
 
 dashboardRouter.get('/', (req, res) => {
-    req.flash('success', 'Signup successful');
-    return res.render('dashboard', { messages: req.flash() });
- }); 
+    try {
+        req.flash('success', 'Signup successful');
+        return res.render('dashboard', { messages: req.flash() });
+    } catch (error) {
+        // Handle the error, for example, by sending an error response or logging it.
+        console.error('An error occurred:', error);
+        return res.status(500).send('An error occurred while rendering the dashboard page.');
+    }
+});
 
-export default dashboardRouter;
+module.exports = dashboardRouter;
