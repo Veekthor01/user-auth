7ae6c1fab553412a9251ccfa01463b67d6623e5d
Veekthor01 <veekthor01@gmail.com>
2023-10-16 16:02:30 +0300

    change randombytes

--- routes/forgotPasswordRoute.js
+++ routes/forgotPasswordRoute.js
@@ -5,8 +5,10 @@ const transporter = require('../email');
 const { connectToDB } = require('../db');
 
 const forgotPasswordRouter = express.Router();
+
 const myEmail = process.env.EMAIL_HOST_USER;
 
+// Function to generate a unique token to be sent by email to the user for password reset
 function generateToken() {
     return crypto.randomBytes(20).toString('hex');
   }
@@ -15,7 +17,6 @@ function generateToken() {
     try {
         res.render('forgotPassword');
     } catch (error) {
-        // Handle the error, for example, by sending an error response or logging it.
         console.error('An error occurred:', error);
         return res.status(500).send('An error occurred while rendering the forgotPassword page.');
     }
@@ -24,9 +25,7 @@ function generateToken() {
 forgotPasswordRouter.post('/', async (req, res) => {
     const { email } = req.body;
     try {
-      // Connect to your MongoDB database
       const db = await connectToDB();
-      // Assuming you have a 'users' collection in your database
       const usersCollection = db.collection('user');
       // Find the user with the specified email
       const user = await usersCollection.findOne({ email });
@@ -34,11 +33,11 @@ forgotPasswordRouter.post('/', async (req, res) => {
         res.send('No user found with this email address.');
         return;
       }
-      // Generate a unique token with an expiration time (e.g., 1 hour from now)
+      // Generate a unique token with an expiration time
       const token = generateToken();
       const expirationTime = new Date();
-      expirationTime.setHours(expirationTime.getHours() + 1); // Set an expiration time (1 hour from now)
-      // Assuming you have a 'passwordResetTokens' collection in your database
+      expirationTime.setHours(expirationTime.getHours() + 1);
+      // Create a collection to store the password reset tokens
       const passwordResetTokensCollection = db.collection('passwordResetTokens');
       // Store the password reset token with its expiration time in your database
       await passwordResetTokensCollection.insertOne({ token, userId: user._id, expirationTime });
@@ -46,7 +45,7 @@ forgotPasswordRouter.post('/', async (req, res) => {
       const resetLink = `http://localhost:3000/reset-password?token=${token}`;
       // Send an email with the reset link
       const mailOptions = {
-        from: myEmail, // Replace with your email
+        from: myEmail,
         to: email,
         subject: 'Password Reset',
         text: `Click this link to reset your password: ${resetLink}`,
@@ -54,15 +53,15 @@ forgotPasswordRouter.post('/', async (req, res) => {
       // Send the email
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-          //console.error('Email could not be sent:', error);
+          console.error('Email could not be sent:', error);
           res.send('Email could not be sent.');
         } else {
-          //console.log('Email sent:', info.response);
+          console.log('Email sent:', info.response);
           res.send('Check your email for a password reset link.');
         }
       });
     } catch (error) {
-      //console.error('Error:', error);
+      console.error('Error:', error);
       res.send('An error occurred while processing your request.');
     }
   });
