feaf7f03f7b118666bbdb3bd4b18f96843fd591d
Veekthor01 <veekthor01@gmail.com>
2023-10-16 16:09:09 +0300

    update sign up routes

--- routes/signUpRoute.js
+++ routes/signUpRoute.js
@@ -9,7 +9,6 @@ signupRouter.get('/', (req, res) => {
     try {
         return res.render('signup', { messages: req.flash() });
     } catch (error) {
-        // Handle the error, for example, by sending an error response or logging it.
         console.error('An error occurred:', error);
         return res.status(500).send('An error occurred while rendering the signup page.');
     }
@@ -21,27 +20,27 @@ signupRouter.post('/', (req, res, next) => {
   // Validate the user input
   if (!validator.isEmail(email)) {
     req.flash('error', 'Invalid email format');
-    return res.render('signup', { messages: req.flash() }); // Redirect to the signup page
+    return res.render('signup', { messages: req.flash() });
   }
   if (!validator.isLength(password, { min: 4 })) {
     req.flash('error', 'Password must be at least 4 characters');
-    return res.render('signup', { messages: req.flash() }); // Redirect to the signup page
+    return res.render('signup', { messages: req.flash() });
   }
   // Pass the request to the authentication middleware
   passport.authenticate('local-signup', (err, user, info) => {
     if (err) {
       req.flash('error', 'Error during signup');
-      return res.render('signup', { messages: req.flash() }); // Redirect to the signup page
+      return res.render('signup', { messages: req.flash() });
     } else if (!user) {
       if (info && info.message) {
         req.flash('error', info.message);
       } else {
         req.flash('error', 'Signup failed');
       }
-      return res.render('signup', { messages: req.flash() }); // Redirect to the signup page
+      return res.render('signup', { messages: req.flash() });
     } else {
       req.flash('success', 'Signup successful');
-      return res.redirect('/dashboard'); // Redirect to the dashboard page
+      return res.redirect('/dashboard');
     }
   })(req, res, next);
 });
