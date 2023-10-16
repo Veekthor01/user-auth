d4cde305f6cfa901d0c2b545e2852a50324d4ab1
Veekthor01 <veekthor01@gmail.com>
2023-10-16 16:10:08 +0300

    update routes code

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -6,7 +6,6 @@ homeRouter.get('/', (req, res) => {
         req.flash("success", "Logout successful");
         res.render('index', { messages: req.flash() });
     } catch (error) {
-        // Handle the error, for example, by sending an error response or logging it.
         console.error('An error occurred:', error);
         return res.status(500).send('An error occurred while rendering the home page.');
     }
--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -7,7 +7,6 @@ logoutRouter.get('/', (req, res) => {
       req.flash('success', 'Logout successful');
       return res.redirect('/');
   } catch (error) {
-      // Handle the error, for example, by sending an error response or logging it.
       console.error('An error occurred:', error);
       return res.status(500).send('An error occurred while logging out.');
   }
